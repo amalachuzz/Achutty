a9b282dab7e59bd17dd88c44f9401730bb7618f5
Amal <90576371+amalachuzz@users.noreply.github.com>
2021-12-15 16:16:16 +0530

    Update database.js

--- plugins/database.js
+++ plugins/database.js
@@ -19,14 +19,14 @@ var plk_here = new Date().toLocaleDateString(get_localized_date)
 var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '```'
 	const buttons = [
 
-        {buttonId: 'id1', buttonText: {displayText: 'OWN\n\n*Amalser v2 two types of alive message*\n\n*Hi i am live {pp}*\n\n*Hi i am alive {qt}*\n\n*welcome message addedd simple way*\n\n*.welcome {pp} {gphead} {gpmaker} {gpdesc}{owner}*\n\n*Another way*\n*.welcome {gif} {gphead} {gpmaker} {gpdesc} {time} {owner}*\n\n*Broadcast adedd replay with any message .bc*\n\n*Alive message time set*\n\n*Button message added .bot and .help*\n\n*All type of downloading command example .yt, .video, .song, .get*\n\n '}, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: 'GIT\n\n```youtube Video: https://youtu.be/5MKycJxmA4c```\n\n```Github link: https://github.com/Amal-ser/Amalser```\n\n```YT_PASSWORD:  password in youtube channel video watch it full and subcribe for more updations```\n\n```For making photo to link - https://t.me/HK_telegraph_BOT```\n'  }, type: 1},
+        {buttonId: 'id1', buttonText: {displayText: 'OWN\n\n*Achutty two types of alive message*\n\n*Hi i am live {pp}*\n\n*Hi i am alive {qt}*\n\n*welcome message addedd simple way*\n\n*.welcome {pp} {gphead} {gpmaker} {gpdesc}{owner}*\n\n*Another way*\n*.welcome {gif} {gphead} {gpmaker} {gpdesc} {time} {owner}*\n\n*Broadcast adedd replay with any message .bc*\n\n*Alive message time set*\n\n*Button message added .bot and .help*\n\n*All type of downloading command example .yt, .video, .song, .get*\n\n '}, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: 'GIT\n\n```Github link: https://github.com/amalachuzz/Achutty```\n\n```YT_PASSWORD: for yt password contact owner Wa.me/917591973073```\n\n```For making photo to link - https://t.me/HK_telegraph_BOT```\n'  }, type: 1},
 
       ]
       
       const buttonMessage = {
-          contentText: ' ʜʏ ᴅᴜᴅᴇ....👋🏻\n\n```BOT NAME:``` *'+Config.BOT+'*\n\n🃏 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🍒 ᴅᴀᴛᴇ : ```' + plk_here + '```\n\n🃏 ᴄʟɪᴄᴋ ᴍᴇɴᴜ ᴀɴᴅ ᴇɴᴊᴏʏ ᴛʜᴇ ʙᴏᴛ\n',
-          footerText: '© Amalser',
+          contentText: ' ʜʏ ᴅᴜᴅᴇ....👋🏻\n\n```BOT NAME:``` *'+Config.BOT+'*\n\n🌺 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🌿 ᴅᴀᴛᴇ : ```' + plk_here + '```\n\n🌺 ᴄʟɪᴄᴋ ᴍᴇɴᴜ ᴀɴᴅ ᴇɴᴊᴏʏ ᴛʜᴇ ʙᴏᴛ\n',
+          footerText: '© Achutty',
           buttons: buttons,
           headerType: 1
       }
